651bf591be35cde812697d16e3503bdc371dd9a7
Szymon Szwedzinski <szymonszwedzinskiii@gmail.com>
2025-05-02 22:04:49 +0200

    Esp code

--- esp32/TemperatureRead/TemperatureRead.ino
+++ esp32/TemperatureRead/TemperatureRead.ino
@@ -1,25 +1,61 @@
+#include <WiFi.h>
+#include <HTTPClient.h>
+#include <ArduinoJson.h>
 
-const int ledPin = 2;
+const char* ssid = "";
+const char* password ="";
+const char* serverUrl = "http://192.168.1.235:8000/api/get_data/";
+
 const int tempraturepin=15;
 float temperature;
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
-  pinMode(ledPin,OUTPUT);
+  WiFi.begin(ssid,password);
   pinMode(tempraturepin,INPUT);
+
+  while(WiFi.status() !=WL_CONNECTED){
+    delay(500);
+    Serial.println(".");
+  }
+  Serial.println("\nPolaczono z WIFI");
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  digitalWrite(ledPin,HIGH);
+
+  if (WiFi.status() == WL_CONNECTED) {
+    HTTPClient http;
+
+    http.begin(serverUrl);
+    http.addHeader("Content-Type", "application/json");
+
   int sensorValue = analogRead(tempraturepin);
   float voltage = sensorValue *(3.3 / 4095.0);
   temperature = voltage * 100.0;
-  Serial.print("Temperatura: ");
-  Serial.print(temperature);
-  Serial.println();
-  delay(1000);
-  digitalWrite(ledPin,LOW);
-  delay(1000);
+
+    StaticJsonDocument<200> doc;
+    doc["device_name"] = "ESP32_Sensor";
+    doc["temperature"] = temperature;
+
+    String requestBody;
+    serializeJson(doc, requestBody);
+
+    int httpResponseCode = http.POST(requestBody);
+    if (httpResponseCode > 0) {
+          String response = http.getString();
+          Serial.print("Odpowiedź serwera: ");
+          Serial.print(httpResponseCode);
+          Serial.println(response);
+          
+        } else {
+          Serial.printf("Błąd POST: %s\n", http.errorToString(httpResponseCode).c_str());
+        }
+
+        http.end();
+      }
+      delay(20000);
+
 }
+
+
